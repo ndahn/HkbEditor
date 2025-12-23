5598bc07f2ba27c29d5bbea7b417a55efc8fa423
Nikolas Dahn <nikolas.dahn@dfki.de>
2025-12-23 23:58:38 +0100

    Fixed new stancemove template, updated xmas icon

--- templates/ER/new_stancemove.py
+++ templates/ER/new_stancemove.py
@@ -3,6 +3,7 @@ from hkb_editor.hkb.hkb_enums import (
     CustomManualSelectorGenerator_AnimeEndEventType as AnimeEndEventType,
     CustomManualSelectorGenerator_OffsetType as CmsgOffsetType,
     CustomManualSelectorGenerator_ChangeTypeOfSelectedIndexAfterActivate as ChangeIndexType,
+    hkbClipGenerator_PlaybackMode as PlaybackMode,
 )
 
 
@@ -36,73 +37,99 @@ def run(
     ----------
     ctx : TemplateContext
         The template context.
+    front : Animation
+        Forward moving animation.
+    back : Animation
+        Backward moving animation.
+    left : Animation
+        Left moving animation.
+    right : Animation
+        Right moving animation.
     """
-    # All stance moves by FS use the same clip node for both blend layers. I tend to avoid
-    # doing this, but it feels like it might be important here
-    anims = {
-        "Front": (front, ctx.new_clip(front)),
-        "Back": (back, ctx.new_clip(back)),
-        "Left": (left, ctx.new_clip(left)),
-        "Right": (right, ctx.new_clip(right)),
-    }
 
-    # Blending transition effects
-    dur0 = ctx.find("name=Duration0")
-    dur7 = ctx.find("name=Duration7")
+    def create_stancemove_blender(sync: bool) -> HkbRecord:
+        # Blending transition effects
+        dur0 = ctx.find("name=Duration0")
+        
+        if sync:
+            dur7 = ctx.find("name=Duration7_Sync")
+            suffix = ""
+        else:
+            dur7 = ctx.find("name=Duration7_Sync00")
+            suffix = "00"
 
-    # motion blend layer
-    motion_cmsgs = []
-    for direction, (anim, clip) in anims.items():
-        cmsg = ctx.new_cmsg(
-            anim,
-            name=f"{name}_{direction}_CMSG",
-            generators=[clip],
-            enableScript=False,
-            enableTae=False,
-            offsetType=CmsgOffsetType.IDLE_CATEGORY,
+        # All stance moves by FS use the same clip node for both blend layers. I tend to avoid
+        # doing this, but it feels like it might be important here
+        anims = {
+            "Front": (front, ctx.new_clip(front, mode=PlaybackMode.LOOPING)),
+            "Back": (back, ctx.new_clip(back, mode=PlaybackMode.LOOPING)),
+            "Left": (left, ctx.new_clip(left, mode=PlaybackMode.LOOPING)),
+            "Right": (right, ctx.new_clip(right, mode=PlaybackMode.LOOPING)),
+        }
+
+        # motion blend layer
+        motion_cmsgs = []
+        for direction, (anim, clip) in anims.items():
+            cmsg = ctx.new_cmsg(
+                anim,
+                name=f"{name}_{direction}_CMSG{suffix}",
+                generators=[clip],
+                enableScript=False,
+                enableTae=False,
+                offsetType=CmsgOffsetType.IDLE_CATEGORY,
+                animeEndEventType=AnimeEndEventType.NONE,
+            )
+            motion_cmsgs.append(cmsg)
+
+        motion_msg = ctx.new_selector(
+            "MoveDirection",
+            name=f"{name}_motion{suffix}",
+            generators=motion_cmsgs,
+            selectedIndexCanChangeAfterActivate=True,
+            generatorChangedTransitionEffect=dur0,
         )
-        motion_cmsgs.append(cmsg)
 
-    motion_msg = ctx.new_selector(
-        "MoveDirection",
-        name=f"{name}_motion",
-        generators=motion_cmsgs,
-        selectedIndexCanChangeAfterActivate=True,
-        generatorChangedTransitionEffect=dur0,
-    )
+        motion_blend_child = ctx.new_blender_generator_child(motion_msg, weight=0.01, worldFromModelWeight=1.0)
 
-    motion_blend_child = ctx.new_blender_generator_child(motion_msg, weight=0.01, worldFromModelWeight=1.0)
+        # anim blend layer
+        anim_cmsgs = []
+        for direction, (anim, clip) in anims.items():
+            cmsg = ctx.new_cmsg(
+                anim, 
+                name=f"{name}_{direction}_anim_CMSG{suffix}",
+                generators=[clip],
+                enableScript=False,
+                offsetType=CmsgOffsetType.IDLE_CATEGORY,
+                animeEndEventType=AnimeEndEventType.NONE,
+            )
+            anim_cmsgs.append(cmsg)
 
-    # anim blend layer
-    anim_cmsgs = []
-    for direction, (anim, clip) in anims.items():
-        cmsg = ctx.new_cmsg(
-            anim, 
-            name=f"{name}_{direction}_anim_CMSG",
-            generators=[clip],
-            enableScript=False,
-            offsetType=CmsgOffsetType.IDLE_CATEGORY,
+        anim_msg = ctx.new_selector(
+            "MoveDirection",
+            name=f"{name}_anime{suffix}",
+            generators=anim_cmsgs,
+            selectedIndexCanChangeAfterActivate=True,
+            generatorChangedTransitionEffect=dur7,
         )
-        anim_cmsgs.append(cmsg)
 
-    anim_msg = ctx.new_selector(
-        "MoveDirection",
-        name=f"{name}_anime",
-        generators=anim_cmsgs,
-        selectedIndexCanChangeAfterActivate=True,
-        generatorChangedTransitionEffect=dur7,
-    )
+        anim_blend_child = ctx.new_blender_generator_child(anim_msg, weight=1.0, worldFromModelWeight=0.0)
 
-    anim_blend_child = ctx.new_blender_generator_child(anim_msg, weight=1.0, worldFromModelWeight=0.0)
+        # Bundle the blend children in a blender generator and add it to the stance move selector
+        return ctx.new_blender_generator(
+            [motion_blend_child, anim_blend_child],
+            blendParameter=1.0,
+            name=f"{name}_Blend{suffix}"
+        )
 
-    # Bundle the blend children in a blender generator and add it to the stance move selector
-    stance_blend = ctx.new_blender_generator(
-        [motion_blend_child, anim_blend_child],
-        blendParameter=1.0,
-        name=f"{name}_Blend"
-    )
-
+    stance_blend = create_stancemove_blender(True)
     stancemove_msg = ctx.find("name=StanceMove_Selector")
     move_type_idx = ctx.array_add(stancemove_msg, "generators", stance_blend)
+
+    stance_blend_nosync = create_stancemove_blender(False)
+    stancemove_msg_nosync = ctx.find("name=StanceMoveNoSync_Selector")
+    move_type_idx_nosync = ctx.array_add(stancemove_msg_nosync, "generators", stance_blend_nosync)
+
+    if move_type_idx != move_type_idx_nosync:
+        ctx.logger.warning("NOTE: children of StanceMove_Selector and StanceMoveNoSync_Selector don't match, this will probably cause problems!")
 
     ctx.logger.info(f"Index of new StanceMoveType {name}: {move_type_idx}")
